65f19e8850b920c282829fea0f71ec3d7fc4d783
decent-token-6050 <garrettburns@mac.com>
2019-08-29 21:27:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,8 +8,13 @@ def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+def introduction_with_language_optional (name,language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language("Cordorellopaccimadacciazzo", "Cambrian")
 
+introduction_with_language_optional("Cordorellopaccimadacciazzo")
 
 
 
